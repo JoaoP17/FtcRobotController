8d0c1abd80cc5437f6dfac1d5bd961511a54ff20
JoaoP17 <joaopedroazevedocosta17@gmail.com>
2025-09-01 12:18:03 -0300

    Testando coisas

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PaodeBatata.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+@Disabled
+@TeleOp
+public class PaodeBatata extends OpMode {
+
+    @Override
+    public void init() {
+        telemetry.speak("Aperta Dnv");
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
